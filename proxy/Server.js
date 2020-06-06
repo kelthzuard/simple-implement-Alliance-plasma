b0412087dff5bbbdb9d6bc25ec5a7c69b30c8826
kelthzuard <a522516259@163.com>
2020-06-06 20:17:47 +0800

    fill outline

--- proxy/Server.js
+++ proxy/Server.js
@@ -1,13 +1,50 @@
-const Node = require('./Node')
+const request = require('request');
+const Node = require('./Node');
 
 class Server{
-    constructor(nodeID, nodeTable) {
+    constructor(nodeID, nodeTable, propsal) {
         this.nodeID = nodeID;
         this.address = nodeTable[nodeID];
+        this.propsal = propsal //提议节点
         this.node = new Node(nodeID, nodeTable);
         this.height = 0;
+        this.round = 0;
         this.lockedValue = null;
-        this.lockedHegiht = -1;
+        this.lockedround = -1;
+        this.recivedVote = [];
+        this.recivedCommit = [];
+    }
+
+    send (address, msg) {
+        request.post(address, {
+            form: {
+                data: JSON.stringify(msg)
+            }
+        })
+    }
+
+    broadcast (msg) {
+
+    }
+
+    prepare() {
+        if (this.propsal == this.nodeID) {
+            //为提议节点,广播区块
+        }else {
+            return;
+        }
+    }
+
+    prevote() {
+
+    }
+
+    precommit() {
+
+    }
+
+    writeBlock() {
+
     }
 }
 Server.timeLimit = 1000; //1sec
@@ -19,6 +56,12 @@ const newServer = (nodeID, nodeTable, app) => {
 
 const setRouter = (app, server) => {
     app.post('/prepare', async(req, res) => {
-        server.prepare()
+        server.prepare();
+    })
+    app.post('/prevote', async(req, res) => {
+        server.prevote();
+    })
+    app.post('/precommit', async(req, res) => {
+        server.precommit();
     })
 }
