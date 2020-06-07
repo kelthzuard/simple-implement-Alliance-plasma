985d3b038e75bc216d224a0f0cc1d99e596ca072
kelthzuard <a522516259@163.com>
2020-06-07 15:40:19 +0800

    update

--- proxy/Node.js
+++ proxy/Node.js
@@ -1,9 +1,33 @@
 // 节点信息
+const nodeRSA = require('node-rsa');
+
 class Node{
-    constructor (nodeID, nodeTable) {
+    constructor (nodeID, pubKey, privateKey,nodeTable) {
         this.nodeID = nodeID;
+        this.pubKey = pubKey
+        this.privateKey = privateKey
         this.nodeTable = nodeTable;
         this.block = [];
+        this.height = 0
+    }
+
+    writeBolck (block) {
+        this.block.append(block);
+        this.height = len(block);
+    }
+
+    sign (msg) {
+        const key = new nodeRSA({ b: 2048 });
+        key.importKey(this.privateKey, "pkcs1-private-pem");
+        const sign = key.sign(msg, "base64");
+        return sign;
+    }
+
+    validSign (msg, signedMsg, pubKey) {
+        const key = new nodeRSA({ b: 2048 });
+        key.importKey(pubKey, "pkcs8-public-pem");
+        const verify = key.verify(msg, signedMsg, "base64");
+        return verify;
     }
 }
 
--- proxy/Server.js
+++ proxy/Server.js
@@ -1,13 +1,18 @@
 const request = require('request');
 const Node = require('./Node');
+const geth = require("../geth");
+const block = require("../block");
+
+class Message{
+    constructor(block, lockedValue, lockedround, )
+}
 
 class Server{
-    constructor(nodeID, nodeTable, propsal) {
+    constructor(nodeID, pubKey, privateKey, nodeTable, propsal) {
         this.nodeID = nodeID;
-        this.address = nodeTable[nodeID];
-        this.propsal = propsal //提议节点
-        this.node = new Node(nodeID, nodeTable);
-        this.height = 0;
+        this.address = nodeTable[nodeID].address;
+        this.propsal = propsal; //提议节点
+        this.node = new Node(nodeID, pubKey, privateKey,nodeTable);
         this.round = 0;
         this.lockedValue = null;
         this.lockedround = -1;
@@ -20,16 +25,32 @@ class Server{
             form: {
                 data: JSON.stringify(msg)
             }
-        })
+        });
     }
 
     broadcast (msg) {
-
+        for (let id in this.node.nodeTable) {
+            if (id != this.nodeID) {
+                this.send(this.node.nodeTable[id].address, msg);
+            }
+        }
     }
 
-    prepare() {
+    propsal() {
         if (this.propsal == this.nodeID) {
             //为提议节点,广播区块
+            if (!this.lockedValue && this.lockedround == -1) {
+                const newBlock = await block.generateNextBlock(geth);
+                this.broadcast({
+                    block: newBlock,
+                    lockedValue: null,
+                    lockedround: -1,
+                    round: 0,
+                    height: length(this.node.height)
+                })
+            } else {
+
+            }
         }else {
             return;
         }
@@ -44,20 +65,17 @@ class Server{
     }
 
     writeBlock() {
-
+        this.node.writeBolck()
     }
 }
 Server.timeLimit = 1000; //1sec
 
-const newServer = (nodeID, nodeTable, app) => {
-    const server = new Server(nodeID, nodeTable);
+const newServer = (nodeID, pubKey, privateKey,nodeTable, app) => {
+    const server = new Server(nodeID, pubKey, privateKey,nodeTable);
     setRouter(app, server);
 };
 
 const setRouter = (app, server) => {
-    app.post('/prepare', async(req, res) => {
-        server.prepare();
-    })
     app.post('/prevote', async(req, res) => {
         server.prevote();
     })
